871d110d2181135d5dd90455ee6d3fb6dafd38a7
chayoosang <yoo7969@naver.com>
2023-07-13 13:44:52 +0900

    Initial Commit

---
+++ meongmory_iOS/meongmory_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  meongmory_iOSApp.swift
+//  meongmory_iOS
+//
+//  Created by 유상 on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct meongmory_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
