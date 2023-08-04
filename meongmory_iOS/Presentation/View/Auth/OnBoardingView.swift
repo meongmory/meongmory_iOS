42df1e1906abdf52325af55d36d0d873045297ea
ryr0121 <ryr0121@gmail.com>
2023-08-04 15:25:38 +0900

    feat: 닉네임 설정 화면, 온보딩 화면 파일 추가

---
+++ meongmory_iOS/Presentation/View/Auth/OnBoardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoardingView.swift
+//  meongmory_iOS
+//
+//  Created by 김초원 on 2023/08/04.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
---
+++ meongmory_iOS/Presentation/View/Auth/SetNicknameView.swift
@@ -0,0 +1,20 @@
+//
+//  SetNicknameView.swift
+//  meongmory_iOS
+//
+//  Created by 김초원 on 2023/08/04.
+//
+
+import SwiftUI
+
+struct SetNicknameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetNicknameView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetNicknameView()
+    }
+}
