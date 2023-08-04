dd6bfd05873b28daf81cce8e0461e7beae88f238
ryr0121 <ryr0121@gmail.com>
2023-08-04 17:54:10 +0900

    feat: 온보딩 내 이미지 asset 추가 및 온보딩 화면 하단 버튼 추가

--- meongmory_iOS/Presentation/View/Auth/OnBoardingView.swift
+++ meongmory_iOS/Presentation/View/Auth/OnBoardingView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct OnBoardingView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            Spacer()
+            bottomButton
+        }
+        .padding(.top, 40)
+        .padding(.horizontal, 20)
+
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    self.presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
+                }
+            }
+        }
+    }
+    
+    var bottomButton: some View {
+        NavigationLink {
+            OnBoardingView()
+        } label: {
+            Text("다음")
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 10)
+        }
+        .navigationBarBackButtonHidden(true)
+        .background(Color(red: 60/255, green: 39/255, blue: 10/255))
+        .cornerRadius(10)
     }
 }
 
