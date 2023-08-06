63609bb26935842e22e88b593d8a8589165fccc3
ryr0121 <ryr0121@gmail.com>
2023-08-07 05:05:02 +0900

    feat: 마이페이지 메인 UI 구현

---
+++ meongmory_iOS/Presentation/View/MyPage/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  meongmory_iOS
+//
+//  Created by 김초원 on 2023/08/07.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
