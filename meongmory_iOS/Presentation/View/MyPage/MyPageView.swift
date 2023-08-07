a7ca157e5c7b5c73c295229c423c3ed9b5810932
ryr0121 <ryr0121@gmail.com>
2023-08-08 02:40:17 +0900

    feat: 로그아웃, 회원탈퇴 alert 추가

--- meongmory_iOS/Presentation/View/MyPage/MyPageView.swift
+++ meongmory_iOS/Presentation/View/MyPage/MyPageView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct MyPageView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
     var body: some View {
-        ScrollView {
-            UserProfilView()
-            MyPageMenuListView()
+        NavigationView {
+            ScrollView {
+                UserProfilView()
+                MyPageMenuListView()
+            }
+            .navigationBarTitle("마이페이지", displayMode: .inline)
         }
-        .navigationBarTitle("마이페이지", displayMode: .inline)
+        
     }
 }
 
@@ -140,9 +145,25 @@ struct MyPageMenuListView: View {
 }
 
 struct MyPageMenuRow: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
     var menuDetail: MenuDetail
+    @State var alertShowing: Bool = false
+    
+    private let logoutMessage = "로그아웃 하시겠습니까?"
+    private let signoutMessage = "회원 탈퇴 시,\n저장된 모든 정보를 잃게 됩니다.\n탈퇴하시겠습니까?"
     
     var body: some View {
+        switch menuDetail {
+        case .logout, .signout :
+            getAlertRow()
+        default:
+            normalRow
+        }
+        
+    }
+    
+    var normalRow: some View {
         NavigationLink {
             menuDetail.view
         } label: {
@@ -156,11 +177,48 @@ struct MyPageMenuRow: View {
                 }
                 Spacer()
             }
-            .frame(height: 20) 
+            .frame(height: 20)
         }
         .listRowSeparator(.hidden)
         .listRowBackground(Color.clear)
     }
+    
+    func getAlertRow() -> some View {
+        var row: some View {
+            Button {
+                alertShowing.toggle()
+            } label: {
+                HStack {
+                    HStack {
+                        Image(menuDetail.iconName)
+                        Text(menuDetail.rawValue)
+                            .font(Font.system(size: 12))
+                            .fontWeight(.medium)
+                            .padding(.leading, 10)
+                    }
+                    Spacer()
+                }
+                .frame(height: 20)
+            }
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color.clear)
+            .alert((menuDetail == .logout) ? "로그아웃" : "회원 탈퇴",
+                   isPresented: $alertShowing) {
+                Button("취소", role: .cancel) {}
+                Button((menuDetail == .logout) ? "로그아웃" : "탈퇴") {
+                    if menuDetail == .logout {
+                        print("로그아웃 요청하기")
+                    } else {
+                        print("회원탈퇴 요청하기")
+                    }
+                }
+            } message: {
+                Text((menuDetail == .logout) ? logoutMessage : signoutMessage)
+            }
+        }
+        return row
+    }
+    
     
     
     
