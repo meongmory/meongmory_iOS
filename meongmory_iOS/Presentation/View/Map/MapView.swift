ecb3cd182ead7e2053799e401c8dc5ce1fbc3ed3
chayoosang <yoo7969@naver.com>
2023-08-07 20:07:41 +0900

    #8 feat: 현재 위치 받아오기 구현

--- meongmory_iOS/Presentation/View/Map/MapView.swift
+++ meongmory_iOS/Presentation/View/Map/MapView.swift
@@ -9,21 +9,17 @@ import SwiftUI
 import NMapsMap
 
 struct MapView: View {
-    @State var coord: (Double, Double) = (0, 0)
     let locationManger = LocationManager()
+    @State var coord: (Double, Double)
+    
+    
+    init() {
+        self.coord = locationManger.getCoordinate()
+    }
     
     var body: some View {
         ZStack {
             NaverMapView(coord: $coord)
-            VStack {
-                Button {
-                    coord = locationManger.getCoordinate()
-                } label: {
-                    Text("현재위치로")
-                }
-                Spacer()
-            }.foregroundColor(Color.black)
-            
         }
     }
 }
@@ -33,28 +29,22 @@ struct NaverMapView: UIViewRepresentable {
 
     func makeUIView(context: Context) -> NMFNaverMapView {
         let view = NMFNaverMapView()
-        view.mapView.touchDelegate = context.coordinator
         view.showZoomControls = false
         view.showLocationButton = true
         view.mapView.positionMode = .direction
         view.mapView.zoomLevel = 17
         
+        let cameraMove = NMFCameraUpdate(scrollTo: NMGLatLng(lat: coord.0, lng: coord.1))
+        cameraMove.animation = .fly
+        cameraMove.animationDuration = 1
+        view.mapView.moveCamera(cameraMove)
+        
         return view
     }
 
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
-        let cameraMove = NMFCameraUpdate(scrollTo: NMGLatLng(lat: coord.0, lng: coord.1))
-        cameraMove.animation = .fly
-        cameraMove.animationDuration = 1
-        uiView.mapView.moveCamera(cameraMove)
     }
     
-    class Coordinator: NSObject, NMFMapViewTouchDelegate {
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        return Coordinator()
-    }
 }
 
 
