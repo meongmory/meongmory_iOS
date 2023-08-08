53601e717d9053187b2f724e7dfcff2aa16c320f
ryr0121 <ryr0121@gmail.com>
2023-08-09 00:04:53 +0900

    fix: 약관 웹뷰 로드 오류 처리

--- meongmory_iOS/Presentation/View/MyPage/MenuDetailView/TermWebView.swift
+++ meongmory_iOS/Presentation/View/MyPage/MenuDetailView/TermWebView.swift
@@ -8,17 +8,15 @@
 import SwiftUI
 
 enum TermType: String {
-    case term = "www.naver.com" // 임시 URL값
-    case privacyTerm = "www.google.com" // 임시 URL값
+    case term = "https://www.naver.com" // 임시 URL값
+    case privacyTerm = "https://www.google.com" // 임시 URL값
 }
 
 struct TermWebView: View {
     var type: TermType?
     
     var body: some View {
-        ScrollView {
-            WebView(url: type?.rawValue ?? "www.naver.com")
-        }
+        WebView(url: type?.rawValue ?? "https://www.naver.com")
         .navigationTitle(type == .term ? "약관 안내" : "개인정보 처리 방침")
     }
 }
