c7a794be321ea865306c25ea2682c2e606fb9990
ryr0121 <ryr0121@gmail.com>
2023-08-08 23:48:56 +0900

    feat: 공지사항 화면 nav bar 추가

--- meongmory_iOS/Presentation/View/MyPage/MenuDetailView/NoticeView.swift
+++ meongmory_iOS/Presentation/View/MyPage/MenuDetailView/NoticeView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct NoticeView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
     var body: some View {
-        Text("NoticeView")
+        VStack {
+            // TODO: 컴포넌트 추가
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationTitle("공지사항")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    self.presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .foregroundColor(Color(red: 51/255, green: 51/255, blue: 51/255))
+                }
+            }
+        }
     }
 }
 
