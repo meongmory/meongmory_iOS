cb8ec3bced4e480a436e03a9e3209a518c4dadca
Nya128 <nanalovesbap@gmail.com>
2023-08-12 18:58:27 +0900

    #14 feat: contentView에 홈뷰 추가

---
+++ meongmory_iOS/Presentation/View/Home/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  meongmory_iOS
+//
+//  Created by 김나연 on 2023/08/12.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ScrollView {
+            // 상단바
+            // 강아지네
+            // 보러가기
+            // 사업자
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
