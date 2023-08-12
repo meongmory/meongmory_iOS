0d9b7457d37e8054af49d806aea441c7c89085d5
Nya128 <nanalovesbap@gmail.com>
2023-08-13 02:13:41 +0900

    #14 feat: 홈화면 navigationBar 구현

--- meongmory_iOS/Presentation/View/Home/HomeView.swift
+++ meongmory_iOS/Presentation/View/Home/HomeView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct HomeView: View {
+    init() {
+        // 스크롤 시 NavigationBar 색상 변경 막음
+        let appearance: UINavigationBarAppearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some View {
-        ScrollView {
-            // 상단바
-            // 강아지네
-            // 보러가기
-            // 사업자
+        NavigationView {
+            ScrollView {
+                // 강아지네
+                
+                // 보러가기
+                // 사업자
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {}, label: {
+                        Image("main_text_icon")
+                    })
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // TODO: 알림 화면으로 이동
+                    }, label: {
+                        Image("bell")
+                    })
+                }
+            }
         }
-        .navigationBarBackButtonHidden(true)
     }
 }
 
