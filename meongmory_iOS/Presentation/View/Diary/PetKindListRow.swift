42212268b3444692876abb7912294e408795b73c
chayoosang <yoo7969@naver.com>
2023-08-14 11:31:09 +0900

    #11 feat: 반려동물 품종 검색 UI 구현

---
+++ meongmory_iOS/Presentation/View/Diary/PetKindListRow.swift
@@ -0,0 +1,44 @@
+//
+//  PetKindListRow.swift
+//  meongmory_iOS
+//
+//  Created by 유상 on 2023/08/13.
+//
+
+import SwiftUI
+
+struct Animal: Identifiable {
+    var id: Int
+    var animalName: String
+    var animalType: String
+    var isSelected: Bool
+}
+
+struct PetKindListRow: View {
+    @State var animal: Animal
+    
+    var body: some View {
+        HStack {
+            Image(animal.animalType == "강아지" ? (animal.isSelected ? "animal_dog_select" : "animal_dog") : (animal.isSelected ? "animal_cat_select": "animal_cat"))
+            
+            Text(animal.animalName + " [\(animal.animalType)]")
+              .font(Font.custom("AppleSDGothicNeoM00", size: 12))
+              .foregroundColor(.black)
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(animal.isSelected ? "animal_checkbox_select" : "animal_checkbox")
+            }
+
+        }
+    }
+}
+
+struct PetKindListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PetKindListRow(animal: Animal(id: 1, animalName: "말티즈", animalType: "강아지", isSelected: false))
+    }
+}
---
+++ meongmory_iOS/Presentation/View/Diary/PetKindView.swift
@@ -0,0 +1,70 @@
+//
+//  PetKindView.swift
+//  meongmory_iOS
+//
+//  Created by 유상 on 2023/08/13.
+//
+
+import SwiftUI
+
+struct PetKindView: View {
+    @State var petKindSelect: String = ""
+    @State var petKind: String = ""
+    
+    @State var petList: [Animal] = [
+        Animal(id: 1, animalName: "말티즈", animalType: "강아지", isSelected: false),
+        Animal(id: 2, animalName: "오드아이", animalType: "고양이", isSelected: false)
+    ]
+    
+    var body: some View {
+        VStack {
+            HStack(spacing: 30) {
+                Button {
+                    petKindSelect = "강아지"
+                } label: {
+                    Text("강아지")
+                        .font(Font.custom("AppleSDGothicNeoM00", size: 12))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(petKindSelect == "강아지" ? .black : Color(red: 0.45, green: 0.45, blue: 0.45))
+
+                }.frame(width: (CGFloat.screenWidth - 47) / 2, height: 39)
+                    .background(petKindSelect == "강아지" ? Color(red: 1, green: 0.98, blue: 0.93) : Color(red: 0.98, green: 0.98, blue: 0.98))
+                    .cornerRadius(10)
+                
+                Button {
+                    petKindSelect = "고양이"
+                } label: {
+                    Text("고양이")
+                        .font(Font.custom("AppleSDGothicNeoM00", size: 12))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(petKindSelect == "고양이" ? .black : Color(red: 0.45, green: 0.45, blue: 0.45))
+                    
+                }.frame(width: (CGFloat.screenWidth - 47) / 2, height: 39)
+                    .background(petKindSelect == "고양이" ? Color(red: 1, green: 0.98, blue: 0.93) : Color(red: 0.98, green: 0.98, blue: 0.98))
+                    .cornerRadius(10)
+                
+            } // 강이지 고양이 선택 버튼
+            
+            ZStack(alignment: .trailing) {
+                TextField("품종 검색", text: $petKind)
+                    .foregroundColor(.clear)
+                    .frame(width: (CGFloat.screenWidth - 32), height: 39)
+                    .background(Color(red: 0.98, green: 0.98, blue: 0.98))
+                    .cornerRadius(10)
+                
+                Image("search")
+                    .padding(.trailing, 10)
+            }
+            
+            List(petList) {
+                PetKindListRow(animal: $0)
+            }
+        }
+    }
+}
+
+struct PetKindView_Previews: PreviewProvider {
+    static var previews: some View {
+        PetKindView()
+    }
+}
